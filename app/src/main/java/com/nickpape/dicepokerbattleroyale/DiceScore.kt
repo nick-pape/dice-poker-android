dbd12c17f548739b90cc6bb3efb4e3311da3792f
Nick Pape <5674316+nick-pape@users.noreply.github.com>
2022-10-17 02:40:10 -0500

    Get a include'd scoresheet semi working

--- app/src/main/java/com/nickpape/dicepokerbattleroyale/DiceScore.kt
+++ app/src/main/java/com/nickpape/dicepokerbattleroyale/DiceScore.kt
@@ -1,13 +1,10 @@
-package com.nickpape.dicepokerbattleroyale.fragments
+package com.nickpape.dicepokerbattleroyale
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.nickpape.dicepokerbattleroyale.R
-import com.nickpape.dicepokerbattleroyale.databinding.FragmentHomeBinding
-import com.nickpape.dicepokerbattleroyale.databinding.FragmentScoresheetBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,16 +13,13 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [ScoresheetFragment.newInstance] factory method to
+ * Use the [DiceScore.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ScoresheetFragment : Fragment() {
+class DiceScore : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
-    private var _binding: FragmentScoresheetBinding? = null
-    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,15 +33,8 @@ class ScoresheetFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentScoresheetBinding.inflate(inflater, container, false)
-
-        binding.scoreOne.diceScoreImage.setImageResource(R.drawable.dice_one)
-        binding.scoreTwo.diceScoreImage.setImageResource(R.drawable.dice_two)
-        binding.scoreThree.diceScoreImage.setImageResource(R.drawable.dice_three)
-        binding.scoreFour.diceScoreImage.setImageResource(R.drawable.dice_four)
-        binding.scoreFive.diceScoreImage.setImageResource(R.drawable.dice_five)
-        binding.scoreSix.diceScoreImage.setImageResource(R.drawable.dice_six)
-        return binding.root
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dice_score, container, false)
     }
 
     companion object {
@@ -57,12 +44,12 @@ class ScoresheetFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment ScoresheetFragment.
+         * @return A new instance of fragment DiceScore.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            ScoresheetFragment().apply {
+            DiceScore().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
